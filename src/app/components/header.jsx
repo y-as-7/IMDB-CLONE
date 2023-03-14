83e65864d222f190cc0e2d4ecfa32f0607f72ec6
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-14 03:43:37 +0200

    first commit

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import MenuItem from './MenuItem'
+import {AiFillHome}from "react-icons/ai"
+import {BsInfoCircleFill} from "react-icons/bs"
+import Link from 'next/link'
 export default function Header() {
   return (
-    <div>header</div>
+    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto py-5 items-center'>
+      <div className='flex'>
+        <MenuItem title="HOME" address ="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address ="/about" Icon={BsInfoCircleFill} />
+      </div>
+      <div>
+        <Link href='/'>
+          <h2 className='text-2xl'>
+            <span className='mr-1 font-bold bg-amber-500 py-1 px-2 rounded '>IMDB</span>
+            <span className='text-1xl hidden sm:inline'>Clone</span></h2>
+        </Link>
+      </div>
+    </div>
   )
 }
