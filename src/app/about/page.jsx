cd8105e25915cb7006bc81077f9c82cff2360f01
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-14 18:48:44 +0200

    create the nav bar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ export default function About() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus architecto quibusdam dolor ipsum neque molestias, aperiam porro, eius harum est explicabo corporis sunt vero quod eaque, eveniet temporibus iste nobis.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias</p>
     </div>
   )
 }
