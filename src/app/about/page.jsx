c37f908a8ad8ba6d55fde6920d15118c72aca5db
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-14 18:07:56 +0200

    about section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 ">
+        <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus architecto quibusdam dolor ipsum neque molestias, aperiam porro, eius harum est explicabo corporis sunt vero quod eaque, eveniet temporibus iste nobis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus atque labore at consequuntur qui, voluptatem culpa necessitatibus dolorum harum? Harum recusandae corrupti quia eaque iure ipsam laborum iusto molestias.</p>
+    </div>
+  )
+}
