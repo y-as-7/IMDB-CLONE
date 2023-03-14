624d2cb8bd2f16d4a213758248ee9e2ab26bf023
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-14 03:42:05 +0200

    create header

---
+++ src/app/layout.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header'
+import './globals.css'
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header/>
+        
+        
+        {children}</body>
+    </html>
+  )
+}
