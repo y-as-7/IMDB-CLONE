8b42e41080284138f8a42df3e8d396bfb86012c1
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-15 03:40:51 +0200

    make movie page

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import React from "react";
 import {FiThumbsUp} from'react-icons/fi'
 export default function Card({ result }) {
+  
   return (
     <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-400 group">
       <Link href={`/movie/${result.id}`}>
+        
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
